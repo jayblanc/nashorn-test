d7308d0d2e8094df9c31f22a53ec1a4350fb6687
Rodislav MOLDOVAN <Rodislav.MOLDOVAN@ext.ec.europa.eu>
2017-10-26 16:12:42 +0200

    playing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,23 @@ public class Main {
         try {
             ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 
-            engine.eval("load('classpath:thenashlib.js');");
+            engine.eval("load('classpath:require.js');");
+            engine.eval("load('classpath:nashlib.js');");
             engine.eval("print('Hello World');");
 
-            engine.eval("eval(Nash)");
+            engine.eval("eval(myNash = new Nash())");
 
-            engine.eval("print(Nash.Nash('sheldon').display('Hello boy'));");
+            engine.eval("print(myNash.display('Hello boy'));");
 
-            Invocable invocable = (Invocable) engine;
-
-            Object result = invocable.invokeFunction("Nash.Nash", "sheldon");
-            System.out.println(result);
-            System.out.println(result.getClass());
-
-            result = invocable.invokeFunction("display", new Date());
-            System.out.println(result);
-            System.out.println(result.getClass());
+//            Invocable invocable = (Invocable) engine;
+//
+//            Object result = invocable.invokeFunction("Nash.Nash", "sheldon");
+//            System.out.println(result);
+//            System.out.println(result.getClass());
+//
+//            result = invocable.invokeFunction("display", new Date());
+//            System.out.println(result);
+//            System.out.println(result.getClass());
 
 
         } catch (Exception e) {
